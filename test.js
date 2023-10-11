29a7c9ddb46a56a6b7ca57d57c78f73322934ca7
Sachin <srivastavsachin2016@gmail.com>
2023-10-11 23:33:36 +0530

    i removed noida

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const accountName = "sachin"
 
 let accountID = "23"
 var accountEmail = "sachin@gmail.com"
-accountCity = "Noida"
+accountCity = ""
 
 console.table([accountName,accountID,accountEmail,accountCity]);
