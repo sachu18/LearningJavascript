18a11303d0cf5a13ddb0680eb64753b611357d58
Sachin <srivastavsachin2016@gmail.com>
2023-10-11 23:29:29 +0530

    this is my first git commit

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("sachin")
+
+const accountName = "sachin"
+
+let accountID = "23"
+var accountEmail = "sachin@gmail.com"
+accountCity = "Noida"
+
+console.table([accountName,accountID,accountEmail,accountCity]);
