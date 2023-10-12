f45ffaa33dba48c0a70be6727cdc92b561341477
Sachin <srivastavsachin2016@gmail.com>
2023-10-12 20:00:54 +0530

    Discuss on  Variables

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("sachin")
-
-const accountName = "sachin"
-
-let accountID = "23"
-var accountEmail = "sachin@gmail.com"
-accountCity = ""
- 
-console.table([accountName,accountID,accountEmail,accountCity]);
